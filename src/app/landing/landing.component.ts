a0d9ee3719005cd6c5c7928d113c690d57753354
Pankaj Phour <pankaj.phour70@gmail.com>
2023-12-07 15:14:44 +0530

    Checking the indexed DB functionality

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IndexedDbService } from '../services/indexed-db.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
@@ -7,14 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  name = 'Angular';
+  key: any;
+  value: any;
+  result: string;
+  form:FormGroup;
+  
+  constructor(public indexedDb: IndexedDbService,private _fb:FormBuilder) { }
+
+  validation(){
+    this.form = this._fb.group({
+      key:['',Validators.required],
+      value: ['',Validators.required]
+    })
+  }
 
   ngOnInit(): void {
+      this.validation();
+      console.log(this.form);
+      this.key = document.getElementById('key');
+      this.value = document.getElementById('value');
   }
 
-  notify(){
-    console.log("Hello from notify function");
-    
+  get() {
+    this.result = 'storing...';
+    this.indexedDb.get('mystore', this.key.value)
+    .subscribe(
+      x => {
+        this.result = `FOUND: ${JSON.stringify(x)}`;
+      },
+      err => {
+        this.result = `ERROR: ${err}`;
+      }
+    );
   }
+  
+  put() {
+    this.result = 'processing...';
+    this.indexedDb.put('mystore', {key: this.key.value, value: this.value.value, ttl: 60*5})
+    .subscribe(
+      x => this.result = `STORED: ${JSON.stringify(x)}`, 
+      err => {
+        this.result = `ERROR: ${err}`;
+      }
+    );
+  }
+
+  // clear() {
+  //   this.result = 'processing...';
+  //   this.indexedDb.clear('mystore')
+  //   .subscribe(
+  //     () => this.result = `CLEARED ALL RECORDS`, 
+  //     err => {
+  //       this.result = `ERROR: ${err}`;
+  //     }
+  //   );
+  // }
 
 }
